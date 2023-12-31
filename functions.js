3e5f226a802bff6a79745b559cc7495e283acf10
shivharchavan <shivharchavan89@gmail.com>
2023-12-30 20:02:26 -0800

    new

---
+++ functions.js
@@ -0,0 +1,27 @@
+var text="hello"
+console.log(text.charAt(0));
+var str1="hello"
+var str2="coding"
+console.log(str1.concat(str2));
+var text="lets find "
+console.log(text.indexOf("find"));
+var text="find the dev knows to dev"
+var pos=text.lastIndexOf("dev")
+console.log( pos);
+var text1="whole world need data engineer"
+console.log(text1.replace("whole","all"));
+const s= new String(`test`);
+console.log(typeof s);
+console.log(s.valueOf());
+console.log(typeof s.valueOf());
+var abc="   the world is beautifull    "
+console.log( "without triming ",abc)
+console.log(abc.trim());
+var a=10
+var b=20
+var c=a+b
+console.log(c);
+
+
+
+
